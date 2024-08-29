07c412b147e987407603f29b1c57a5029b139865
LiliyaKostadinova <costalilyy@gmail.com>
2024-08-30 01:28:07 +0300

    add a listing model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# Listing model 
+class Listing(models.Model):
+    '''Model class to describe the Listing object and its attributes.'''
+    TYPE_CHOICES = [
+        ('for_rent', 'For Rent'),
+        ('for_sale', 'For Sale'),
+    ]
+    is_published = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)
+    
+    title = models.CharField(max_length=200)
+    type = models.CharField(choices=TYPE_CHOICES, max_length=20)
+    street_address = models.CharField(max_length=300)
+    city = models.CharField(max_length=100)
+    price = models.FloatField(max_length=100)
+    lot_size = models.FloatField(max_length=100)
+    story = models.IntegerField(max_length=2)
+    nr_rooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1, blank=True)
+    building_year = models.IntegerField(blank=True)
+    description = models.TextField(blank=True)
+    
+    # Select the field which will be displayed when looking for the object
+    def __str__(self):
+        return self.title 
